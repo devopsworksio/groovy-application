02a317c1b5438b74c01d103d6b0f6d7bbc725257
neil <neil@thump.org>
2016-10-16 12:30:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Gradle Application Plugin
+
+Create a distributable with some empty directories
+
+This is the best I could come up with
---
+++ src/main/java/org/thump/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.thump;
+
+/**
+ * Created by neil on 15/10/16.
+ */
+public class HelloWorld {
+    public static void Main (String Args[]) {
+        System.out.println("Hello World");
+    }
+}
