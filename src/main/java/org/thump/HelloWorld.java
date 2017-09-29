ee553de99c41b79021d62fe5789ff0cbd065aaf7
neil.tingley <neil@winder-it.co.uk>
2017-09-29 11:27:30 +0100

    - added support for artifactory

--- src/main/java/org/thump/HelloWorld.java
+++ src/main/java/org/thump/HelloWorld.java
@@ -3,8 +3,8 @@ package org.thump;
 /**
  * Created by neil on 15/10/16.
  */
-public class HelloWorld {
-    public static void Main (String Args[]) {
-        System.out.println("Hello World");
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
     }
 }
